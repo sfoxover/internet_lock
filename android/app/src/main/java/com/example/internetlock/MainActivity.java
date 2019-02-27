1231f5449aa5a9da6656e6609f6b1b03d95a0a5e
sfoxover <sfoxover@gmail.com>
2019-02-27 08:38:08 -0800

    - check in progress

--- android/app/src/main/java/com/example/internetlock/MainActivity.java
+++ android/app/src/main/java/com/example/internetlock/MainActivity.java
@@ -5,9 +5,20 @@ import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+  // Channel name for flutter call
+  private static final String CHANNEL = "com.sfoxover.internetlock/lockapp";
+  
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+     new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+                new MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(MethodCall call, Result result) {
+                        getActivity().startLockTask();
+                    }
+                });
   }
 }
